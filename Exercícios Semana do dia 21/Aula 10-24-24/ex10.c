14354b9b535ecdd0595dca629345c0f6a53894ee
ApenasOLinco <lincoln.matos1003@gmail.com>
2024-10-25 17:10:58 -0300

    Add ex10

---
+++ Exercícios Semana do dia 21/Aula 10-24-24/ex10.c
@@ -0,0 +1,17 @@
+/*
+    Uma empresa possui três lojas, cada uma com 6 funcionários.
+    Escreva um programa que forneça à gerência algumas informações relativas
+    aos salários dos funcionários dessa empresa. Os salários devem ser
+    armazenados em uma matriz bidimensional (loja X funcionário). O programa
+    deve:
+        a. preencher a matriz de salários por leitura do teclado;
+        b. imprimir os salários de todos os funcionários, identificando qual a loja em que trabalha;
+        c. imprimir o total pago em salários por loja;
+        d. informar quantos funcionários recebem salário superior a R$2.000,00 na primeira loja;
+        e. informar a média salarial da segunda loja
+*/
+
+/*
+    O exercício 10 dessa lista é o mesmo que o exercício 5 da lista da pasta "Aula 10-21-24".
+    É possível consultar a resolução dele por lá!
+*/
